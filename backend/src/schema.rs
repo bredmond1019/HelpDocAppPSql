650aff8eb949559052c0c5f5b5dc00eed392c937
Brandon Redmond <brandon@gethealthie.om>
2024-11-02 15:45:16 -0300

    Updated Schema

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,5 +1,67 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::sql_types::SqlType, diesel::query_builder::QueryId)]
+    #[diesel(postgres_type(name = "embedding_type"))]
+    pub struct EmbeddingType;
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::*;
+
+    article_chunks (id) {
+        id -> Uuid,
+        article_id -> Uuid,
+        content -> Text,
+        is_title -> Bool,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::*;
+
+    article_search_metadata (id) {
+        id -> Uuid,
+        article_id -> Uuid,
+        summary -> Text,
+        bullet_points -> Array<Nullable<Text>>,
+        keywords -> Array<Nullable<Text>>,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::*;
+
+    articles (id) {
+        id -> Uuid,
+        collection_id -> Uuid,
+        title -> Text,
+        slug -> Text,
+        content -> Text,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::*;
+
+    collections (id) {
+        id -> Uuid,
+        name -> Text,
+        description -> Nullable<Text>,
+        slug -> Text,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
+
 diesel::table! {
     use diesel::sql_types::*;
     use pgvector::sql_types::*;
@@ -12,6 +74,21 @@ diesel::table! {
         #[max_length = 255]
         edited_by -> Nullable<Varchar>,
         created_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::*;
+    use super::sql_types::EmbeddingType;
+
+    embeddings (id) {
+        id -> Uuid,
+        source_id -> Uuid,
+        embedding_type -> EmbeddingType,
+        embedding -> Vector,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
     }
 }
 
@@ -93,13 +170,21 @@ diesel::table! {
     }
 }
 
+diesel::joinable!(article_chunks -> articles (article_id));
+diesel::joinable!(article_search_metadata -> articles (article_id));
+diesel::joinable!(articles -> collections (collection_id));
 diesel::joinable!(content_versions -> old_articles (article_id));
 diesel::joinable!(old_article_chunks -> old_articles (article_id));
 diesel::joinable!(old_articles -> old_collections (collection_id));
 diesel::joinable!(old_embeddings -> old_articles (article_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    article_chunks,
+    article_search_metadata,
+    articles,
+    collections,
     content_versions,
+    embeddings,
     old_article_chunks,
     old_articles,
     old_collections,
